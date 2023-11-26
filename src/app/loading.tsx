fa86903e331dfba53595d3469ada4314ae342d30
Serhi <intern.nine@uwp.digital>
2023-11-26 20:11:47 +0200

    ❤️

---
+++ src/app/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className='bg-slate-400 h-[100vh] w-[100vw] flex items-center justify-center flex-col '>
+      <h1 className='text-white text-4xl mx-auto text-center mb-4'>Loading</h1>
+    </div>
+  )
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className='bg-black h-[100vh] w-[100vw] flex items-center justify-center flex-col '>
+      <h1 className='text-white text-4xl mx-auto text-center mb-4'>Page Not Found(</h1>
+      <a href='/' className="text-lg text-sky-600 underline">Home</a>
+    </div>
+  )
+}
